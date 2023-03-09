ed46fb23e92c7b51cfa95fb56d97272e807a7365
Vizlab <dominykm@stud.ntnu.no>
2023-03-09 19:34:46 +0100

    Added basic selection of objects

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    private GameObject ObjectSelectionManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ObjectSelectionManager = GameObject.Find("ObjectSelectionManager");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Selected()
+    {
+        if (!GameObject.ReferenceEquals(ObjectSelectionManager.GetComponent<ObjectSelection>().selectedObject, this.gameObject))
+        {
+
+            ObjectSelectionManager.GetComponent<ObjectSelection>().ChangeSelectedObject(this.gameObject);
+
+            this.gameObject.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+        }
+
+        Debug.Log("Select");
+    }
+
+    public void DeSelected()
+    {
+        Debug.Log("Deselect");
     }
 }
---
+++ Assets/ObjectSelection.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelection : MonoBehaviour
+{
+
+    public GameObject selectedObject;
+
+    public GameObject editObjectCanvas;
+    public GameObject leftControllerCanvas;
+
+
+    public void showEditCanvas()
+    {
+        leftControllerCanvas.SetActive(false);
+        editObjectCanvas.SetActive(true); 
+    }
+
+    public void showLeftControllerCanvas()
+    {
+        editObjectCanvas.SetActive(false);
+        leftControllerCanvas.SetActive(true);
+    }
+
+    public void ChangeSelectedObject(GameObject newSelectedObject)
+    {
+        if (selectedObject != null) selectedObject.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+
+        selectedObject = newSelectedObject;
+
+        showEditCanvas();
+    }
+
+    public void RemoveSelection()
+    {
+        selectedObject.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+        selectedObject = null;
+
+        showLeftControllerCanvas();
+    }
+}
